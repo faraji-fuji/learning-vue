33ecc81e42e4eb566a6b7aa5e5f71ac3868da20d
faraji-fuji <farajifuji@gmail.com>
2023-05-21 23:00:30 +0300

    attribute bindings

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,39 @@ export default {
       counter: {
         count: 0
       },
-      titleClass: 'title'
+      titleClass: 'title',
+      count: 0
+    }
+  },
+
+  methods: {
+    increment(){
+      // update componet state
+      this.count++;
+      this.counter.count++;
+    },
+
+    say_hi(){
+      alert("Hi faraji")
     }
   }
 }
 </script>
 
 <template>
-  <h1 v-bind:class="titleClass">{{ message }}</h1> <!-- add a dynamic class binding here -->
-  <p :class="titleClass">{{ message2 }}</p>
+  <!-- Declarative rendering -->
+  <h1>{{ message }}</h1>
   <h2>Count is: {{ counter.count }}</h2>
+
+  <!-- Attribute binding  -->
+  <p :class="titleClass">{{ message2 }}</p>
+  <h1 v-bind:class="titleClass">{{ message }}</h1> <!-- add a dynamic class binding here -->
+
+
+  <!-- Event listeners -->
+  <button v-on:click="increment">count is {{ counter.count }}</button>
+  <button @click="increment">count is {{ counter.count }}</button>
+  <button v-on:click="say_hi">Say HI</button>
 </template>
 
 
